f8283a79ba31228955791734517f2b6f27e6cec6
Paul <paulmeyer1996@gmail.com>
2020-01-31 16:41:24 +0900

    Rough beta build
    This is a full version of the game. It has a couple of bugs that can be fixed but by and large it is feature complete.
    This commit added a main menu screen that will open when the game is run. From there you can decide what level to play. When you win or lose the game you a new option shows up in the game over/ you win scree that will take you back to the main menu. This project is going to be shelved for a little bit while I move to the 3D section of the Udemy course.

--- HeistMeisters/Interface/SplashScreen.gd
+++ HeistMeisters/Interface/SplashScreen.gd
@@ -2,12 +2,13 @@ extends CanvasLayer
 
 
 
-func _on_TryAgain_pressed():
+func _on_PlayTutorial_pressed():
+	get_tree().change_scene("res://Levels/TutorialLevel.tscn")
+
+
+func _on_Level1_pressed():
 	get_tree().change_scene("res://Levels/Level1.tscn")
 
 
 func _on_Quit_pressed():
 	get_tree().quit()
-
-func _on_Main_Menu_pressed():
-	get_tree().change_scene("res://Interface/SplashScreen.tscn")
